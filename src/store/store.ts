9791a6aa31515171ef274e72faeced25e48f0f26
Rahul-Narayanasamy <rahulkishore227@gmail.com>
2022-08-26 01:29:13 +0530

    removed unused values

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice"
 import productReducer from "./features/products/productSlice";
 import cartReducer from "./features/cart/cartSlice";
